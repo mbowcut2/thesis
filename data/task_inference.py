3286fdfd4dbfb22cabcc43565ca1b3d8a3f370ba
mbowcut2 <mbowcut@gmail.com>
2024-09-12 18:01:09 -0600

    added code task template to inference sandbox

--- data/task_inference.py
+++ data/task_inference.py
@@ -20,7 +20,7 @@ def get_chat_template(system_prompt, user_message):
 
 
 def get_model_output(input, model, tokenizer):
-    input_ids = tokenizer.encode(input, return_tensors='pt').to(DEVICE)
+    input_ids = tokenizer.encode(input, return_tensors='pt').to(model.device)
 
     output = model.generate(input_ids, max_length=200)
 
@@ -35,16 +35,29 @@ if __name__ == '__main__':
     # parser.add_argument('--input', type=str, default='The meaning of life is')
     parser.add_argument('--model', type=str, default='Llama-2-7b-hf')
     parser.add_argument('--system_prompt', type=str, default='You are a helpful AI assistant.')
+    parser.add_argument('--coding_prompt', type=str, default='')
     args = parser.parse_args()
 
-    model = AutoModelForCausalLM.from_pretrained(LLAMA_PATH + args.model).to(DEVICE)
+    model = AutoModelForCausalLM.from_pretrained(
+        LLAMA_PATH + args.model,
+        device_map= 'auto'
+    )
     tokenizer = AutoTokenizer.from_pretrained(LLAMA_PATH + args.model)
 
     while True:
+        if args.coding_prompt == 'factual':
+            coding_prompt = 'What is the name of a python package or library that can be used to '
+        elif args.coding_prompt == 'neutral':
+            coding_prompt = 'Give me the name of a python package that can be used to '
+        elif args.coding_prompt == 'creative':
+            coding_prompt = 'Make up the name for a python package that could be used to '
+        else:
+            coding_prompt = ''
         my_input = input('Enter your input: ')
         if my_input == 'exit':
             break
-        chat_input = get_chat_template(args.system_prompt, my_input)
+        prompt = coding_prompt + my_input
+        chat_input = get_chat_template(args.system_prompt, prompt)
         get_model_output(chat_input, model, tokenizer)
 
 
