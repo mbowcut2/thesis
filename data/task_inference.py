b2d48e9471a88879f04cfeeb93e86ae3979c536a
mbowcut2 <mbowcut@gmail.com>
2024-09-12 18:52:08 -0600

    added file inference

--- data/task_inference.py
+++ data/task_inference.py
@@ -5,6 +5,9 @@ import torch.nn as nn
 
 import argparse
 
+import json
+from tqdm import tqdm
+
 LLAMA_PATH = '../../llama2/'
 DEVICE = 'cuda:5'
 
@@ -26,8 +29,7 @@ def get_model_output(input, model, tokenizer):
 
     # breakpoint()
 
-    print(tokenizer.decode(output[0], skip_special_tokens=True))
-    return 
+    return tokenizer.decode(output[0], skip_special_tokens=True) 
 
 
 if __name__ == '__main__':
@@ -36,6 +38,9 @@ if __name__ == '__main__':
     parser.add_argument('--model', type=str, default='Llama-2-7b-hf')
     parser.add_argument('--system_prompt', type=str, default='You are a helpful AI assistant.')
     parser.add_argument('--coding_prompt', type=str, default='')
+    parser.add_argument('--interactive', action='store_true')
+    parser.add_argument('--input_file_path', type=str, default='')
+    parser.add_argument('--output_file_path', type=str, default='output.json')
     args = parser.parse_args()
 
     model = AutoModelForCausalLM.from_pretrained(
@@ -44,21 +49,41 @@ if __name__ == '__main__':
     )
     tokenizer = AutoTokenizer.from_pretrained(LLAMA_PATH + args.model)
 
-    while True:
-        if args.coding_prompt == 'factual':
-            coding_prompt = 'What is the name of a python package or library that can be used to '
-        elif args.coding_prompt == 'neutral':
-            coding_prompt = 'Give me the name of a python package that can be used to '
-        elif args.coding_prompt == 'creative':
-            coding_prompt = 'Make up the name for a python package that could be used to '
+    if args.coding_prompt == 'factual':
+        coding_prompt = 'What is the name of a python package or library that can be used to '
+    elif args.coding_prompt == 'neutral':
+        coding_prompt = 'Give me the name of a python package that can be used to '
+    elif args.coding_prompt == 'creative':
+        coding_prompt = 'Make up the name for a python package that could be used to '
+    else:
+        coding_prompt = args.coding_prompt
+
+    if args.interactive:
+        print('Type "exit" to quit')
+        while True:
+            my_input = input(f'{coding_prompt} \nEnter your input: ')
+            if my_input == 'exit':
+                break
+            prompt = coding_prompt + my_input
+            chat_input = get_chat_template(args.system_prompt, prompt)
+            print(get_model_output(chat_input, model, tokenizer))
+
+    else:
+        if args.input_file_path:
+            print(f'Running inference on {args.model} with {args.input_file_path} and coding prompt: {coding_prompt}')
+            with open(args.input_file_path, 'r') as f:
+                tasks = json.load(f)
+                outputs = []
+                for task in tqdm(tasks):
+                    prompt = coding_prompt + task.lower()
+                    chat_input = get_chat_template(args.system_prompt, prompt)
+                    outputs.append(get_model_output(chat_input, model, tokenizer))
+
+            with open(args.output_file_path, 'w') as f:
+                json.dump(outputs, f)
         else:
-            coding_prompt = ''
-        my_input = input('Enter your input: ')
-        if my_input == 'exit':
-            break
-        prompt = coding_prompt + my_input
-        chat_input = get_chat_template(args.system_prompt, prompt)
-        get_model_output(chat_input, model, tokenizer)
+            print('Please specify an input file or run in interactive mode')
+
 
 
     # input = args.input
