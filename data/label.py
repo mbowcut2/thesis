fd81c8ef1c545099930d50395cf93a8de145e3fb
mbowcut2 <mbowcut@gmail.com>
2024-09-30 16:10:02 -0600

    more labeling fun

--- data/label.py
+++ data/label.py
@@ -16,18 +16,29 @@ class LabeledQuery(BaseModel):
     query: str
     isTrue: bool
 
-def get_prompt(response):
+def get_chat_prompt(response):
     return f"The following is a conversation with an AI assistant. Is the information provided by the AI correct? If the AI indicates that it doesn't know, then please mark as correct.\n\n {response}"
 
-def label(queries_and_responses, model="gpt-4o-2024-08-06"):
+def get_base_prompt(response):
+    question = response.split('\n')[0]
+    answer = response.split('\n')[1]
+    q_a_pair = ''.join([question, '\n', answer])
+    return f"The following is a question and answer response from an AI assistant. Is the information provided by the AI correct? If the AI indicates that it doesn't know, then please mark as correct.\n\n {q_a_pair}"
+
+def label(queries_and_responses, openai_model="gpt-4o-2024-08-06", model=''):
     print("Labeling queries and responses...")
     labeled_data = []
     for query, response in tqdm(queries_and_responses):
+        if 'chat' in args.model:
+            message = get_chat_prompt(response)
+        else:
+            message = get_base_prompt(response)
+
         completion = client.beta.chat.completions.parse(
-            model=model,
+            model=openai_model,
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": get_prompt(response)},
+                {"role": "user", "content": message},
             ],
             temperature=0.1,
             response_format=LabeledQuery,
@@ -57,7 +68,7 @@ if __name__ == '__main__':
     with open(input_file_path, 'r') as f:
         queries_and_responses = json.load(f)
     
-    labeled_data = label(queries_and_responses, model=args.openai_model)
+    labeled_data = label(queries_and_responses, openai_model=args.openai_model, model=args.model)
 
     output_file_path = get_output_file_path(args, 'labeled') + '.json'
     print(f"Saving labeled outputs to {output_file_path}")
